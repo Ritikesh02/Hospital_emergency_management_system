a0bc31e0d462a64726af1ef2482d0aa485818ace
Your Name <DELL@DESKTOP-MPP4VTJ>
2025-04-18 10:53:47 +0530

    last commit

--- medical_records/models.py
+++ medical_records/models.py
@@ -4,10 +4,10 @@ from django.utils.timezone import now
 class MedicalRecord(models.Model):
     patient_name = models.CharField(max_length=100)
     description = models.TextField()
-    # file = models.FileField(upload_to='medical_records/')  # Ensure this name is correct
+    
     file = models.FileField(upload_to="medical_records/", blank=True, null=True)
 
-    upload_date = models.DateTimeField(default=now)  # Auto-assign current timestamp
+    upload_date = models.DateTimeField(default=now)  
 
     def __str__(self):
         return self.patient_name
